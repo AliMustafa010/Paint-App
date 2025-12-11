03d97fa8e2ee05bc0fbf62d8123a843014773a5a
AliMustafa010 <alimustafakhan121@gmail.com>
2025-12-11 21:58:19 +0500

    Added dotenv and applied

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 5000;
+const PORT = process.env;
 
 app.use(cors());
 app.use(express.json());

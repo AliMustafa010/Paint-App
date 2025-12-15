0095ccee72ee202983f0e01c257541726d5c016e
AliMustafa010 <alimustafakhan121@gmail.com>
2025-12-15 23:01:57 +0500

    Home created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Router } from "react-router-dom";
 import Home from "./Pages/Home/Home"
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
 
   return (
     <>
+      <Header />
       <Router>
         <Route path="/" element={<Home />} />
       </Router>
+      <Footer />
     </>
   )
 }
